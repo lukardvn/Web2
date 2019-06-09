1151c0af9e6cfc69d0efc500fdf8302eff8ae537
mladjonis <djomlanebitno@gmail.com>
2019-06-09 17:19:57 +0200

    update modela and seed for departures

--- WebApp/WebApp/Models/DomainEntities/Departures.cs
+++ WebApp/WebApp/Models/DomainEntities/Departures.cs
@@ -12,12 +12,17 @@ namespace WebApp.Models.DomainEntities
 
         //public bool Type {get;set;} //gradski ili prigradski ??
 
-        //mozda moze i kao poseban tip,ali recimo da su 0,1,2 vrednosti da ne komplikujemo bar za sada
-        public int DayInWeek { get; set; }
+        ////mozda moze i kao poseban tip,ali recimo da su 0,1,2 vrednosti da ne komplikujemo bar za sada
+        //public int DayInWeek { get; set; } // u timetable ce biti stavljene sve pa kasnije u angularu samo filter
+        //jednostavnije je...
 
         public string TimeTable { get; set; }
 
+        public DateTime ValidFrom { get; set; }
+
         //da li da ostane vise ili samo jedna?
-        public virtual ICollection<TransportLine> TransportLines { get; set; }
+        public string TransportLineID { get; set; }
+
+        public virtual TransportLine TransportLines { get; set; }
     }
 }
